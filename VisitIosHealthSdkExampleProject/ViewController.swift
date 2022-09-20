daa80ead6735901d2efb2768c70c4867d8d886b2
Yash <yash-vardhan@hotmail.com>
2022-09-20 05:36:53 +0530

    Initial Commit

---
+++ VisitIosHealthSdkExampleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VisitIosHealthSdkExampleProject
+//
+//  Created by Yash on 20/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
